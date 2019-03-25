dfb64301f13aa4a0b6f9b334958de04de75b52d2
Fawad Halim <fawad@fawad.net>
2019-03-25 11:29:42 -0500

    Pass in optional greeting

--- hello/hello.py
+++ hello/hello.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 import sys
+import os
 
 who = "world"
+greeting = "hello"
+
+if "GREETING" in os.environ:
+    greeting = os.environ["GREETING"]
+
 if len(sys.argv) > 1:
     who = sys.argv[1]
-print(f"hello {who}")
+print(f"{greeting}, {who}")
